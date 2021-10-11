eb479b40e0154c83c2b1cfd4459d8e2a251f0f14
MiThCeKi <michaeltcking@me.com>
2021-10-11 15:04:40 -0230

    adding in a workflow plan to execute. This is a TODO list of sorts, which will be later deleted. This is what I think I have to do.

--- pages/views.py
+++ pages/views.py
@@ -6,10 +6,24 @@ from googletrans import Translator
 import random
 
 # Create your views here.
+#######----Plan----#######
+#1)It looks like I have to set up a dynamic URL in the main urls.py file. 
+# This is set with <parameter name here> and also a type is specified before it. The
+# syntax is <int: guessednumber> where guessednumber is the parameter.
+#2)Then I have to make sure the name of this parameter is specified in the view. To
+# do this this parameter is included as an argument along side the request. 
+#3)This parameter should be saved in the database so it can be called on in the template.
+# The purpose of calling on the guess is to show the student what they guessed vs the correct
+# answer. There is some possibility that I'll create a username and password so users can
+# have a record of their correct and incorrect answers. 
+#4) I wonder if it would be cool to have record of correct guesses in their profile.
+
+
 counter = 0
 done_already = [1]
 
 def index(request):
+    #this part generates the random number, the translation, and times played counter.
     global counter
     random_number = random.randint(1,50)
     print(random_number)
@@ -20,6 +34,7 @@ def index(request):
     french_number = translator.translate(english_number, src='en', dest='fr')
     french_number_text = french_number.text
     counter += 1
+    #this part renders the webpage 
     context = {
         'english_number': english_number,
         'french_number_text': french_number_text,
