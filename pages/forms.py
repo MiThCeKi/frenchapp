2bcee5117873e7142360ef15dc38e5423a885b36
MiThCeKi <michaeltcking@me.com>
2021-10-13 11:39:43 -0230

    fixing presentation of text box. Adding max character length and removing label to the left

--- pages/forms.py
+++ pages/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 
 class Attempt_Form(forms.Form):
-    attempt = forms.CharField(max_length=200)
+    attempt = forms.CharField(label = '', max_length=200, )
     def clean_attempt(self):
-        attempt = self.cleaned_data.get("attempt")
+        attempt_clean = self.cleaned_data.get("attempt")
         if len(attempt) > 10:
             raise forms.ValidationError("you're typing way too much - just a printed number please")
         
